5a1ea188a145eb3a60f1c50f812315b13e730d09
rafaeldscordeiro <rafael.dscordeiro@gmail.com>
2024-01-29 18:36:38 -0300

    Atualização de livros - sucesso - obs id funcionou com id1 e nao com id11

--- src/test/java/com/rafaelprojetos/libraryapi/api/resource/BookControllerTest.java
+++ src/test/java/com/rafaelprojetos/libraryapi/api/resource/BookControllerTest.java
@@ -188,7 +188,45 @@ public class BookControllerTest {
 
     }
 
+    @Test
+    @DisplayName("Deve atualizar um livro")
+    public void updateBookTest() throws Exception {
+        Long id = 1L;
+        String json = new ObjectMapper().writeValueAsString(createNewBook());
 
+        Book updatingBook = Book.builder().id(1L).title("some title").author("some author").isbn("321").build();
+        BDDMockito.given( service.getById(id)).willReturn( Optional.of(updatingBook));
+        Book updatedBook = Book.builder().id(id).author("Artur").title("As Aventuras").isbn("321").build();
+        BDDMockito.given(service.update(updatingBook)).willReturn(updatedBook);
+
+        MockHttpServletRequestBuilder request = MockMvcRequestBuilders
+                .put(BOOK_API.concat("/" + 1))
+                .content(json)
+                .accept(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON);
+        mvc.perform(request)
+                .andExpect( status().isOk())
+                .andExpect(jsonPath("id").value(id))
+                .andExpect(jsonPath("title").value(createNewBook().getTitle()))
+                .andExpect(jsonPath("author").value(createNewBook().getAuthor()))
+                .andExpect(jsonPath("isbn").value("321"));
+    }
+    @Test
+    @DisplayName("Deve retornar 404 ao tentar atualizar um livro inexistente")
+    public void updateInexistetBookTest() throws Exception {
+
+        String json = new ObjectMapper().writeValueAsString(createNewBook());
+
+        BDDMockito.given( service.getById(Mockito.anyLong())).willReturn( Optional.empty());
+
+        MockHttpServletRequestBuilder request = MockMvcRequestBuilders
+                .put(BOOK_API.concat("/" + 1))
+                .content(json)
+                .accept(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON);
+        mvc.perform(request)
+                .andExpect( status().isNotFound());
+    }
     private BookDTO createNewBook() {
         return BookDTO.builder().author("Artur").title("As Aventuras").isbn("001").build();
     }
